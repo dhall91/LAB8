3521d826f02cf9d931f3ffadb3c04189fcaa1544
Dwayne Hall <dwayne.hall@islandroutes.com>
2020-12-01 13:53:29 -0500

    LAB8 update

--- LAB8.js
+++ LAB8.js
@@ -1,62 +1,28 @@
-var i = 0;
 var kickOut=7;
-var imageStar
+var star = document.getElementById("star");
+var inputMsgElem = document.getElementById("inputMsg");
+
 
 while(kickOut > 0)
 {
+    if (kickOut == 0) break;
+    alert('Enter num between 0 and 7')
+    var input = prompt("You have " + kickOut + " tries.");
 
-   var input = prompt("Enter num here. you have " + kickOut + " left");
-   var star = document.createElement("star");
-   //var img = document.createElement("img");
-        //star.src = "C:/Users/Training/Downloads/Star.png";
-       // img.src = "image.png";
-var src = document.getElementById(star);
-
- 
-   if(input > 0 && input <= 7)
-   {
-
-                    if(input==1){
-                    alert("You're a 1 general");
-                    document.write()
-                    src.appendChild(star);
-                    
-
-                    }else if (input==2){
-                    document.write("You a 2 star general ");
-                    
-
-
-                    }else if (input==3){
-                    document.write("You a 3 star general");
-
-                    }else if (input==4){
-                    document.write("You a 4 star general");
-
-                    }else if (input==5){
-                    document.write("You a 5 star general");
-
-                    }else if (input==6){
-                    document.write("You a 6 star general");
-
-                    }else if (input==7){
-                    document.write("So Sobolious... SEEEEEVEEEEN!!!!!");
-                    
-                }
-
-               break;	
-       }else
-	{
-		
-        //;i++;
-        
-        var answer = kickOut-1;
-        kickOut--;
-		//
-     //  document.getElementById('crap').innerText="You have "+kickOut+ " tries remaining.";
+    if (input > 0 && input <= 7 && input !== null){
+        inputMsgElem.innerHTML = `You're a ${input} star general`;
+        star.style.display = "inline";
+        break;
     }
     
-//i++;    
+    kickOut--;  
+    
 }
 
-//document.write(" You have "+kickOut+ " tries remaining. ");
+if (typeof input !== "undefined"){
+    for (let i = 0; i < input-1; i++) {
+        var imageClone = star.cloneNode(true);
+        document.getElementById("starContainer").appendChild(imageClone);                
+    }
+}
+
